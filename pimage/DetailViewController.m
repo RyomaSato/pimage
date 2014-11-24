0de56168b75e9a8279439c707c98d441ea413252
RyomaSato <maro.nseed221@gmail.com>
2014-11-24 15:16:58 +0800

    変化なし

--- pimage/DetailViewController.m
+++ pimage/DetailViewController.m
@@ -37,7 +37,7 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
     kNumImages = _documentImageList.count;
     
     // UIViewの生成1119
-    uv = [[UIView alloc] initWithFrame:CGRectMake(0,0,kScrollObjWidth*kNumImages,kScrollObjHeight)];
+    uv = [[UIView alloc] initWithFrame:CGRectMake(0,64,kScrollObjWidth*kNumImages,kScrollObjHeight)];//(navigationBarが上から64)
     [self.view addSubview:uv];
     
     //kの初期値設定1119
@@ -45,30 +45,8 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
     //jの初期値設定(ページと数字がかぶらないように設定)1119
     j = kNumImages;
     
-    //ボタン生成1116
-    UIBarButtonItem *nvgRightBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(add)];
-    self.navigationItem.rightBarButtonItem = nvgRightBarButton;
-    
-    //ツールバーボタンの作成
-    //スペーサーの作成
-    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-    
-    UIBarButtonItem *toolLeftBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind target:self action:@selector(back)];
-    
-    UIBarButtonItem *toolRightBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward target:self action:@selector(forward)];
-    
-    NSArray *items = [NSArray arrayWithObjects:spacer, toolLeftBarButton, spacer, spacer, spacer, toolRightBarButton, spacer, nil];
-    self.toolbarItems = items;
+    [self setButton];
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
 //1119
 //    //ScrollViewの生成と設定
 //    scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
@@ -123,6 +101,30 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
 }
 
 
+
+//ボタン生成//1119
+-(void)setButton{
+    
+    //ナビゲーションボタン作成1116
+    UIBarButtonItem *nvgRightBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(add)];
+    self.navigationItem.rightBarButtonItem = nvgRightBarButton;
+    
+    //ツールバーボタンの作成
+    //スペーサーの作成
+    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    UIBarButtonItem *toolLeftBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind target:self action:@selector(back)];
+    
+    UIBarButtonItem *toolRightBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward target:self action:@selector(forward)];
+    
+    NSArray *items = [NSArray arrayWithObjects:spacer, toolLeftBarButton, spacer, spacer, spacer, toolRightBarButton, spacer, nil];
+    self.toolbarItems = items;
+
+    
+}
+
+
+
 - (void)layoutScrollImages
 {
     UIImageView *view = nil;
@@ -136,7 +138,7 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
         if ([view isKindOfClass:[UIImageView class]] && view.tag > 0)
         {
             CGRect frame = view.frame;
-            frame.origin = CGPointMake(curXLoc, 0);//(navigationBarは上から64)
+            frame.origin = CGPointMake(curXLoc, 0);
             view.frame = frame;
             
             curXLoc += (kScrollObjWidth);
@@ -161,8 +163,7 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
         // [UIView setAnimationDelay:3.0];  // 秒後にアニメーションを開始する
         // [UIView setAnimationRepeatCount:5.0];  // アニメーションを5回繰り返す
         [UIView setAnimationCurve:UIViewAnimationCurveLinear];  // アニメーションは一定速度
-        //self.view.center = CGPointMake(-3/2*kScrollObjWidth, 1/2*kScrollObjHeight);  // 終了位置を200,400の位置に指定する
-//        self.view.transform = CGAffineTransformMakeTranslation(-(k-2)*kScrollObjWidth, 0);
+        // self.view.transform = CGAffineTransformMakeTranslation(-(k-2)*kScrollObjWidth, 0);
         uv.transform = CGAffineTransformMakeTranslation(-(k-2)*kScrollObjWidth, 0);
         
         [UIView commitAnimations];  // アニメーション開始
@@ -187,7 +188,6 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
         // [UIView setAnimationDelay:3.0];  // 3秒後にアニメーションを開始する
         // [UIView setAnimationRepeatCount:5.0];  // アニメーションを5回繰り返す
         [UIView setAnimationCurve:UIViewAnimationCurveLinear];  // アニメーションは一定速度
-        //self.view.center = CGPointMake(-3/2*kScrollObjWidth, 1/2*kScrollObjHeight);  // 終了位置を200,400の位置に指定する
         //self.view.transform = CGAffineTransformMakeTranslation(-k*kScrollObjWidth, 0);
         uv.transform = CGAffineTransformMakeTranslation(-k*kScrollObjWidth, 0);
 
@@ -221,14 +221,6 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
     pinImageView.userInteractionEnabled = YES;
     pinImageView.tag = j;//1119
     [imgview addSubview:pinImageView];
-    
-    
-    
-//    /* ドラッグ */
-//    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(view_Dragged:)];
-//    // ビューにジェスチャーを追加
-//    [imgview addGestureRecognizer:pan];
-    
 
 }
 
@@ -251,28 +243,14 @@ const CGFloat kScrollObjWidth  = 320.0;//1pageの幅
     
     }else{
         
-        UIImageView* imgview1 = (UIImageView*)[touch view];
+        UIImageView* touch_imgview = (UIImageView*)[touch view];
         
-        UIImageView* spe_imgview = (UIImageView*)[imgview viewWithTag:imgview1.tag];
+        UIImageView* spe_imgview = (UIImageView*)[imgview viewWithTag:touch_imgview.tag];
         
         spe_imgview.transform = CGAffineTransformMakeTranslation(point.x - spe_imgview.center.x , point.y - spe_imgview.center.y);
     }
 }
 
-
-
-//-(void)view_Dragged:(UIPanGestureRecognizer *)sender{
-//    
-//    // ユーザがドラッグした座標を取得
-//    CGPoint point = [sender translationInView:imageView];
-//    
-//    CGPoint movedPoint = CGPointMake(pinImageView.center.x + point.x ,pinImageView.center.y + point.y);
-//    
-//    pinImageView.center = movedPoint;
-//    
-//    [sender setTranslation:CGPointZero inView:imageView];
-//    
-//}
 
 
 - (void)didReceiveMemoryWarning {
