399a219f546bb8b35e10d478782df61067591f51
RyomaSato <maro.nseed221@gmail.com>
2014-12-18 21:45:40 +0800

    とりあえず完成

---
+++ pimage/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  pimage
+//
+//  Created by 佐藤凌馬 on 2014/12/18.
+//  Copyright (c) 2014年 RyomaSato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ pimage/InfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  InfoViewController.m
+//  pimage
+//
+//  Created by 佐藤凌馬 on 2014/12/18.
+//  Copyright (c) 2014年 RyomaSato. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
